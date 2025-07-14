7975ee52281f90f793cdd7f0f74d51628182a47f
Patrick Rein <patrick.rein@pik-potsdam.de>
2025-07-14 16:53:57 +0200

    Applies autoformatting

--- R/swR.R
+++ R/swR.R
@@ -1,13 +1,13 @@
 #' swR
-#' 
+#'
 #' Function to write R code to a \code{"\linkS4class{swStream}"} object.
-#' 
+#'
 #' Method designed to write the output of R commands to a swStream object,
 #' taking care that it is correctly embedded. As \code{func} argument, you can
 #' pass either the function itself or its name as a string. User defined
 #' functions will only work with option one, two is only for back
 #' compatibility.
-#' 
+#'
 #' @param stream The \code{swStream} object to be modified.
 #' @param func The R command, that shall be ecexuted. Can be specified as a
 #' string for back compatibility
@@ -20,67 +20,71 @@
 #' \code{"\linkS4class{swStream}"},\code{\link{swopen}},\code{\link{swclose}},\code{\link{swlatex}},\code{\link{swtable}},\code{\link{swfigure}}
 #' @examples
 #' \dontrun{
-#' test<-swopen(outfile="test.pdf")
-#' swR(test,print,ls)
-#' swR(test,print,"bla_blubb")
-#' x<-c(1,2,3)
-#' swR(test,print,paste(x,"bla"))
+#' test <- swopen(outfile = "test.pdf")
+#' swR(test, print, ls)
+#' swR(test, print, "bla_blubb")
+#' x <- c(1, 2, 3)
+#' swR(test, print, paste(x, "bla"))
 #' swclose(test)
 #' # Only for back compatibility
-#' swR(test,"print","bla_blubb")
+#' swR(test, "print", "bla_blubb")
 #' }
-#' 
-#' 
+#'
+#'
 # write R code to a sweaveStream object:
 # define the function and its arguments
-# <<"option">>= 
-# "func(...)" 
+# <<"option">>=
+# "func(...)"
 # @
-swR<-function(stream,func,...,option = "echo=FALSE"){
-  envir<-stream
-  stream<-get("stream",envir=envir,inherits=FALSE)
+swR <- function(stream, func, ..., option = "echo=FALSE") {
+  envir <- stream
+  stream <- get("stream", envir = envir, inherits = FALSE)
   orig_stream <- stream
   stufftowrite <- NULL
   if (inherits(stream, "swStream")) {
-    tryCatch({
-      pos<-length(stream@functions)
-      args<-list(...)
-      
-      #save func (necessary in case, it is user defined)
-      if(is.function(func)){
-        stream@functions[[pos+1]]<-func
-      } else{ #needed for back compatibility
-        stream@functions[[pos+1]]<-get(func,pos=1,inherits=TRUE)
+    tryCatch(
+{
+      pos <- length(stream@functions)
+      args <- list(...)
+
+      # save func (necessary in case, it is user defined)
+      if (is.function(func)) {
+        stream@functions[[pos + 1]] <- func
+      } else { # needed for back compatibility
+        stream@functions[[pos + 1]] <- get(func, pos = 1, inherits = TRUE)
       }
-      #paste the command
-      stufftowrite<-paste("stream@functions[[",pos+1,"]](",sep="")
-      if(length(args)>0){
-        #save all arguments as a list in the objects slot of the stream object.
-        stream@arguments[[pos+1]]<-args
-        #paste the arguments in the appropriate format
-        for(i in 1:length(args)){
-          if(any(names(args)!="")){
-            if(names(args)[i]!=""){
-              stufftowrite<-paste(stufftowrite,names(stream@arguments[[pos+1]])[i],"=","stream@arguments[[",pos+1,"]][[",i,"]],",sep="")
-            }else{
-              stufftowrite<-paste(stufftowrite,"stream@arguments[[",pos+1,"]][[",i,"]],",sep="")
+      # paste the command
+      stufftowrite <- paste("stream@functions[[", pos + 1, "]](", sep = "")
+      if (length(args) > 0) {
+        # save all arguments as a list in the objects slot of the stream object.
+        stream@arguments[[pos + 1]] <- args
+        # paste the arguments in the appropriate format
+        for (i in 1:length(args)) {
+          if (any(names(args) != "")) {
+            if (names(args)[i] != "") {
+              stufftowrite <- paste(stufftowrite, names(stream@arguments[[pos + 1]])[i], "=", "stream@arguments[[", pos + 1, "]][[", i, "]],", sep = "")
+            } else {
+              stufftowrite <- paste(stufftowrite, "stream@arguments[[", pos + 1, "]][[", i, "]],", sep = "")
             }
-          }else{
-            stufftowrite<-paste(stufftowrite,"stream@arguments[[",pos+1,"]][[",i,"]],",sep="")
+          } else {
+            stufftowrite <- paste(stufftowrite, "stream@arguments[[", pos + 1, "]][[", i, "]],", sep = "")
           }
         }
       }
-      #remove last comma
-      if(substr(stufftowrite,nchar(stufftowrite),nchar(stufftowrite))==",") stufftowrite<-substr(stufftowrite,1,nchar(stufftowrite)-1)
-      #add options and closing bracket
-      stufftowrite<-paste(stufftowrite,")",sep="")
-      stufftowrite<-c(paste("<<",option,">>=",sep=""),stufftowrite,"@")
-    }, error=function(e){warning("Error occurred in swR, stream will be set back to previous value.\n   Error message: ",e$message,call. = FALSE);stream <- orig_stream})
+      # remove last comma
+      if (substr(stufftowrite, nchar(stufftowrite), nchar(stufftowrite)) == ",") stufftowrite <- substr(stufftowrite, 1, nchar(stufftowrite) - 1)
+      # add options and closing bracket
+      stufftowrite <- paste(stufftowrite, ")", sep = "")
+      stufftowrite <- c(paste("<<", option, ">>=", sep = ""), stufftowrite, "@")
+    },
+ error = function(e) {
+warning("Error occurred in swR, stream will be set back to previous value.\n   Error message: ", e$message, call. = FALSE)
+stream <- orig_stream
+})
     # Save the updated stream object
-    assign("stream",stream,envir = envir)
-    swlatex(envir,stufftowrite)
-    }else{
-      stop("Input is not a sweaveStream!")
-    }
+    assign("stream", stream, envir = envir)
+    swlatex(envir, stufftowrite)
+  } else {
+    stop("Input is not a sweaveStream!")
+  }
 }
- 
--- R/swfigure.R
+++ R/swfigure.R
@@ -1,10 +1,10 @@
 #' swfigure
-#' 
+#'
 #' Function to add R plots to a \code{"\linkS4class{swStream}"} object.
-#' 
+#'
 #' Method designed to add an R plot to a swStream object, taking care that it
 #' is correctly embedded.
-#' 
+#'
 #' @param stream The \code{swStream} object to be modified.
 #' @param plot_func The R command, that produces the plot.
 #' @param ... The arguments of the plotting command.
@@ -26,44 +26,43 @@
 #' \code{"\linkS4class{swStream}"},\code{\link{swopen}},\code{\link{swclose}},\code{\link{swlatex}},\code{\link{swR}},\code{\link{swtable}}
 #' @examples
 #' \dontrun{
-#' test<-swopen(outfile="test.pdf")
-#' swfigure(test,"plot",0,0,tex_caption="test figure",fig.width=0.5)
+#' test <- swopen(outfile = "test.pdf")
+#' swfigure(test, "plot", 0, 0, tex_caption = "test figure", fig.width = 0.5)
 #' swclose(test)
 #' }
-swfigure<-function(stream,plot_func,...,tex_caption="",tex_label="",fig.placement="H",fig.width="",fig.orientation="portrait",sw_option="",sw_label="AUTO"){
-  envir<-stream
-  stream<-get("stream",envir=envir,inherits=FALSE)
+swfigure <- function(stream, plot_func, ..., tex_caption = "", tex_label = "", fig.placement = "H", fig.width = "", fig.orientation = "portrait", sw_option = "", sw_label = "AUTO") {
+  envir <- stream
+  stream <- get("stream", envir = envir, inherits = FALSE)
   if (inherits(stream, "swStream")) {
+    # each plot has to have a different label. AUTO will take care of that
+    if (sw_label == "") stop("Each plot needs a different label to make things work. If you don't specify any label, it will be taken care of automatically")
+    if (sw_label == "AUTO") sw_label <- length(stream@functions) + 1
 
-    #each plot has to have a different label. AUTO will take care of that
-    if(sw_label=="") stop("Each plot needs a different label to make things work. If you don't specify any label, it will be taken care of automatically")
-    if(sw_label=="AUTO") sw_label=length(stream@functions)+1
-    
-    #remember the names of the plot files to delete them afterwards.
-    stream@auxfiles<-c(stream@auxfiles,paste(strsplit(stream@name,"\\.pdf")[[1]],"-",sw_label,".",c("eps","pdf"),sep=""))
-    assign("stream",stream,envir = envir)    
-    #Add the orientation and the options
-    if(fig.orientation=="landscape") sw_label<-paste(sw_label,",width=11.69",sep="")
-    if(sw_option!="")sw_label<-paste(sw_label,",",sw_option,sep="")
-    
+    # remember the names of the plot files to delete them afterwards.
+    stream@auxfiles <- c(stream@auxfiles, paste(strsplit(stream@name, "\\.pdf")[[1]], "-", sw_label, ".", c("eps", "pdf"), sep = ""))
+    assign("stream", stream, envir = envir)
+    # Add the orientation and the options
+    if (fig.orientation == "landscape") sw_label <- paste(sw_label, ",width=11.69", sep = "")
+    if (sw_option != "") sw_label <- paste(sw_label, ",", sw_option, sep = "")
+
     # change the figure width
-    if(fig.width!="") swlatex(envir,stufftowrite=paste("\\setkeys{Gin}{width=",fig.width,"\\textwidth}"))
-    
-    #build figure environment and put the plot command
-    startplot<-"\\begin{figure}"
-    if(fig.placement!="") startplot<-paste(startplot,"[",fig.placement,"]",sep="")
-    startplot<-c(startplot,"\\begin{center}")
-    swlatex(envir,startplot)
-    swR(envir,plot_func,...,option=paste(sw_label,",fig=TRUE,echo=FALSE",sep=""))
-    endplot<-"\\end{center}"
-    if(tex_caption!="") endplot<-c(endplot,paste("\\caption{",gsub("$","\\$",tex_caption,fixed=TRUE),"}",sep=""))
-    if(tex_label!="") endplot<-c(endplot,paste("\\label{",gsub("$","\\$",tex_label,fixed=TRUE),"}",sep=""))
-    endplot<-c(endplot,"\\end{figure}")
-    swlatex(envir,endplot)
-    
+    if (fig.width != "") swlatex(envir, stufftowrite = paste("\\setkeys{Gin}{width=", fig.width, "\\textwidth}"))
+
+    # build figure environment and put the plot command
+    startplot <- "\\begin{figure}"
+    if (fig.placement != "") startplot <- paste(startplot, "[", fig.placement, "]", sep = "")
+    startplot <- c(startplot, "\\begin{center}")
+    swlatex(envir, startplot)
+    swR(envir, plot_func, ..., option = paste(sw_label, ",fig=TRUE,echo=FALSE", sep = ""))
+    endplot <- "\\end{center}"
+    if (tex_caption != "") endplot <- c(endplot, paste("\\caption{", gsub("$", "\\$", tex_caption, fixed = TRUE), "}", sep = ""))
+    if (tex_label != "") endplot <- c(endplot, paste("\\label{", gsub("$", "\\$", tex_label, fixed = TRUE), "}", sep = ""))
+    endplot <- c(endplot, "\\end{figure}")
+    swlatex(envir, endplot)
+
     # change the figure width back to normal again
-    if(fig.width!="") swlatex(envir,stufftowrite=paste("\\setkeys{Gin}{width=1\\textwidth}"))
-  }else{
+    if (fig.width != "") swlatex(envir, stufftowrite = paste("\\setkeys{Gin}{width=1\\textwidth}"))
+  } else {
     print("ERROR! Input is not a sweaveStream!")
   }
 }
