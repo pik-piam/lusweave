d3b4d28b9b4c22dec3c59056c2e14340ba267fe7
Patrick R <hi@patrickrein.de>
2025-07-16 14:49:01 +0200

    Adds condition to skip swclose test on r-universe, as their LaTeX setup is uknown and seemingly incompatible

--- tests/testthat/test-swclose.R
+++ tests/testthat/test-swclose.R
@@ -1,5 +1,6 @@
 test_that("swclose works", {
   skip_if(Sys.which("pdflatex") == "", "pdflatex was not found")
+  skip_if(Sys.getenv("UNIVERSE_NAME") == "pik-piam", "test is incompatible with r-universe LaTeX setup")
   withr::with_tempdir({
     pdfDocument <- swopen(outfile = "test.pdf")
     swlatex(pdfDocument, "tttteeeesssstttt")
